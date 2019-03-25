7c4b5795e0a287046b66b27895d75d8204b91a81
Giovanni Pezzino <G.Pezzino@elsevier.com>
2019-03-25 15:17:15 +0000

    Enlightened!

--- koans/koan05_pointers.cpp
+++ koans/koan05_pointers.cpp
@@ -29,8 +29,8 @@ void Koan05_pointers::they_are_just_variables()
 {
   int an_int = 42;
   int *pointer_to_an_int = &an_int;
-  ASSERT_EQUAL( *pointer_to_an_int, FILL_THE_NUMBER_IN );
-  ASSERT_EQUAL( pointer_to_an_int, THIS_IS_NOT_NULL );
+  ASSERT_EQUAL( *pointer_to_an_int, 42 );
+  ASSERT( pointer_to_an_int != NULL );
 }
 
 void Koan05_pointers::they_are_really_just_variables()
@@ -38,35 +38,35 @@ void Koan05_pointers::they_are_really_just_variables()
   int an_int = 42;
   int another_int = 21;
   int *pointer_to_an_int = &an_int;
-  ASSERT_EQUAL( *pointer_to_an_int, FILL_THE_NUMBER_IN );
-  ASSERT_EQUAL( pointer_to_an_int, THIS_IS_NOT_NULL );
+  ASSERT_EQUAL( *pointer_to_an_int, 42 );
+  ASSERT( pointer_to_an_int != NULL );
   pointer_to_an_int = &another_int;
-  ASSERT_EQUAL( *pointer_to_an_int, FILL_THE_NUMBER_IN );
-  ASSERT_EQUAL( pointer_to_an_int, THIS_IS_NOT_NULL );
+  ASSERT_EQUAL( *pointer_to_an_int, 21 );
+  ASSERT( pointer_to_an_int != NULL );
 }
 
 void Koan05_pointers::they_have_power()
 {
   int an_int = 42;
   int *powerful_pointer = &an_int;
-  ASSERT_EQUAL( *powerful_pointer, FILL_THE_NUMBER_IN );
-  ASSERT_EQUAL( powerful_pointer, THIS_IS_NOT_NULL );
+  ASSERT_EQUAL( *powerful_pointer, 42 );
+  ASSERT( powerful_pointer!= NULL);
   *powerful_pointer = 21;
-  ASSERT_EQUAL( an_int, FILL_THE_NUMBER_IN );
-  ASSERT_EQUAL( powerful_pointer, THIS_IS_NOT_NULL );
+  ASSERT_EQUAL( an_int, 21 );
+  ASSERT( powerful_pointer!= NULL);
 }
 
 void Koan05_pointers::they_are_not_almighty()
 {
-  const int an_int = 42;
-  const int *wannabe_powerful = &an_int;
-  ASSERT_EQUAL( *wannabe_powerful, FILL_THE_NUMBER_IN );
-  ASSERT_EQUAL( wannabe_powerful, THIS_IS_NOT_NULL );
+   int an_int = 42;
+   int *wannabe_powerful = &an_int;
+  ASSERT_EQUAL( *wannabe_powerful, 42 );
+  ASSERT( wannabe_powerful!= NULL );
   // Will this work? Think about it!
   // What do you need to change to make it work?
-//   *wannabe_powerful = 21;
-  ASSERT_EQUAL( an_int, FILL_THE_NUMBER_IN );
-  ASSERT_EQUAL( wannabe_powerful, THIS_IS_NOT_NULL );
+   *wannabe_powerful = 21;
+  ASSERT_EQUAL( an_int, 21 );
+  ASSERT( wannabe_powerful != NULL );
 }
 
 // EOF
